dba615ed74d6d6eea56e22884dc1f7efeb95d471
rahul <rahulravi2502@gmail.com>
2022-03-15 12:30:23 +0530

    Add; Added the method to insert employee payroll database

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,3 +10,9 @@ create Table employee_payroll(
  salary float,
  startdate date,
 );
+
+---UC3 Create Employee payroll database service as a part of CURD operation---
+INSERT INTO employee_payroll (Name, Salary, Startdate) VALUES ('Mohammed', 45986.58, CAST('2018-02-02' as Date));
+INSERT INTO employee_payroll  VALUES ('Sajid', 95986.58, getdate());
+INSERT INTO employee_payroll  VALUES ('Sakil', 685986.58, getdate());
+INSERT INTO employee_payroll (Name, startdate) VALUES ('Roy', getdate());
